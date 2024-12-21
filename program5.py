b24b4a3e9abfd47338f5db7af40825afe9704e2c
ALAN VARGHESE <146810549+Alan-varghese@users.noreply.github.com>
2024-12-21 23:56:16 +0530

    Add files via upload

---
+++ program5.py
@@ -0,0 +1,7 @@
+import numpy as np
+def creatematrix(mc):
+    print("Array"+str(mc)+"Elements")
+    array_1=map(int,input().split())
+    array_1=np.array(list(array_1))
+    print("Array"+str(mc)+"Row Column:")
+    
